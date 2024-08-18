814535704d9aa90ea68f25f4c2139dd3c58d6a97
andreysiznet <andriktatkov@gmail.com>
2024-08-18 11:57:44 +0300

    Update module_5_2.py

--- module_5_2.py
+++ module_5_2.py
@@ -4,10 +4,10 @@ class House:
         self.number_of_floors = number_of_floors
 
     def __len__(self):
-        return (self.number_of_floors)
+        return self.number_of_floors
 
     def __str__(self):
-        return (f"Название: {self.name}, кол-во этажей: {self.number_of_floors}")
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
 
 
 h1 = House('ЖК Эльбрус', 10)
